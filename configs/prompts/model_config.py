e2d4a4f8bf6991ed3ac36ac1483d4bea1806dca5
Humanpredator <info@reality.org.in>
2023-08-23 13:54:18 +0530

    Update model_config.py

--- configs/prompts/model_config.py
+++ configs/prompts/model_config.py
@@ -107,7 +107,7 @@ def config_obj(prompt, config_id=None) -> DictConfig:
                                  }
                   }
     GhibliBackground = {'GhibliBackground': {'base': os.path.join(os.getcwd(), 'models', 'DreamBooth_LoRA',
-                                                                  'CounterfeitV30_30.safetensors'),
+                                                                  'CounterfeitV30_v30.safetensors'),
                                              'path': os.path.join(os.getcwd(), 'models', 'DreamBooth_LoRA',
                                                                   'lora_Ghibli_n3.safetensors'),
                                              'motion_module': [
