6eee88d80878fd0b40477519f5269c41ba9e6269
Alex-dev-1 <91024989+Alex-dev-1@users.noreply.github.com>
2023-09-07 17:49:14 +0530

    duration -> fps

--- animatediff/utils/util.py
+++ animatediff/utils/util.py
@@ -12,7 +12,6 @@ from einops import rearrange
 
 def save_videos_grid(videos: torch.Tensor, path: str, rescale=False, n_rows=6, fps=8):
     videos = rearrange(videos, "b c t h w -> t b c h w")
-
     outputs = []
     for x in videos:
         x = torchvision.utils.make_grid(x, nrow=n_rows)
@@ -21,9 +20,9 @@ def save_videos_grid(videos: torch.Tensor, path: str, rescale=False, n_rows=6, f
             x = (x + 1.0) / 2.0  # -1,1 -> 0,1
         x = (x * 255).numpy().astype(np.uint8)
         outputs.append(x)
-    print(outputs)
+
     os.makedirs(os.path.dirname(path), exist_ok=True)
-    imageio.mimsave(path, outputs, duration=fps)
+    imageio.mimsave(path, outputs, fps=fps)
 
 
 # DDIM Inversion
