2be6a7756b2955d68f78567c0f158932c6f94ccf
Alex-dev-1 <91024989+Alex-dev-1@users.noreply.github.com>
2023-09-07 17:10:27 +0530

    Small Fixes

--- animatediff/utils/util.py
+++ animatediff/utils/util.py
@@ -12,7 +12,7 @@ from einops import rearrange
 
 def save_videos_grid(videos: torch.Tensor, path: str, rescale=False, n_rows=6, fps=8):
     videos = rearrange(videos, "b c t h w -> t b c h w")
-    print(list(videos))
+
     outputs = []
     for x in videos:
         x = torchvision.utils.make_grid(x, nrow=n_rows)
@@ -21,7 +21,7 @@ def save_videos_grid(videos: torch.Tensor, path: str, rescale=False, n_rows=6, f
             x = (x + 1.0) / 2.0  # -1,1 -> 0,1
         x = (x * 255).numpy().astype(np.uint8)
         outputs.append(x)
-
+    print(outputs)
     os.makedirs(os.path.dirname(path), exist_ok=True)
     imageio.mimsave(path, outputs, duration=fps)
 
